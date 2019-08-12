40777662d3cf20a15cbbe808138f9d30283ed0ef
lihkg5mo <54020129+lihkg5mo@users.noreply.github.com>
2019-08-12 16:08:59 +0800

    Add files via upload

---
+++ fifty-cents.js
@@ -0,0 +1,154 @@
+function getFiftyCents() {
+	return [
+	  {
+		uid: 271687,
+		name: 'Hkcollege',
+		topics: [
+		  {
+			tid: 1439203,
+			link: 'https://lihkg.com/thread/1439203/page/1?ref=sharer&post=1',
+			topic: '對付有刀嘅黑社會，我地只可用一樣嘢。',
+			msg: '用氣槍啊屌',
+			exp: '用氣槍 = 叫對方用實彈'
+		  }
+		]
+	  },
+	  {
+		uid: 297660,
+		name: 'Keroro_Tom',
+		topics: [
+		  {
+			tid: 1440989,
+			link: 'https://lihkg.com/thread/1440989/page/1?ref=sharer&post=22',
+			topic: '(有片有gif) 黑警抽水渣個白衫女仔屎忽 請瘋傳! 屌你老母痴撚線一定係特登',
+			msg: '佢手拎Laser.... 應該係被捉原因',
+			exp: '屈個女仔手拎Laser'
+		  }
+		]
+	  },
+	  {
+		uid: 36049,
+		name: 'Westaytgt',
+		topics: [
+		  {
+			tid: 1442750,
+			link: 'https://lihkg.com/thread/1442750/page/79?ref=sharer&post=1951',
+			topic: '「即時討論」港島東現場',
+			msg: '北角打緊!@!',
+			exp: '試圖帶風向，呃人去北角'
+		  }
+		]
+	  },
+	  {
+		uid: 262170,
+		name: '不是暴動',
+		topics: [
+		  {
+			tid: 1443382,
+			link: 'https://lihkg.com/thread/1443382/page/1?ref=sharer&post=1',
+			topic: '有義士俾福建佬圍毆',
+			msg: '4:00:已確認頭先俾四個福建佬圍毆灰衫男係哨兵，警察拉左一個打人紅衫上車就開車走左，灰衫哨兵應該逃走左，其餘打人3個係帶上狗車！',
+			exp: '毫無証據証明灰衫男係哨兵，試圖帶風向，呃人去北角'
+		  }
+		]
+	  },
+	  {
+		uid: 183386,
+		name: '佐維',
+		topics: [
+		  {
+			tid: 1443507,
+			link: 'https://lihkg.com/thread/1443507/page/1?ref=sharer&post=1',
+			topic: '女義士好靚',
+			msg: '',
+			exp: '公開被捕女途人照片及資料'
+		  }
+		]
+	  },
+	  {
+		uid: 95155,
+		name: '都市夜歸人',
+		topics: [
+		  {
+			tid: 1444205,
+			link: 'https://lihkg.com/thread/1444205/page/15?ref=sharer&post=356',
+			topic: '其實今次運動 已經正式進入一個死結',
+			msg: '開學啦，返學啦乖！睇住你地既暴亂開始無所作為時，真係好L心涼！',
+			exp: '不作解釋'
+		  }
+		]
+	  },
+	  {
+		uid: 269498,
+		name: '緊急救援',
+		topics: [
+		  {
+			tid: 1444564,
+			link: 'https://lihkg.com/thread/1444564/page/16?ref=sharer&post=396',
+			topic: '「即時討論」葵涌現場',
+			msg: '突發 葵芳已上實彈，準備攻入新都會及葵涌廣場',
+			exp: '發放假消息'
+		  }
+		]
+	  },
+	  {
+		uid: 271434,
+		name: '真的香港人',
+		topics: [
+		  {
+			tid: 1446406,
+			link: 'https://lihkg.com/thread/1446406/page/1?ref=sharer&post=22',
+			topic: '（禍必及妻兒）行動升級，組小隊伏擊休班警同佢地屋企人！',
+			msg: '反而跪警察宿舍門口道歉認錯，軟化警察對示威者態度，有無得唸？',
+			exp: '不作解釋'
+		  }
+		]
+	  },
+	  {
+		uid: 73856,
+		name: '大熊大',
+		topics: [
+		  {
+			tid: 1448166,
+			link: 'https://lihkg.com/thread/1448166/page/1?ref=sharer&post=12',
+			topic: '荃灣有人叫救命',
+			msg: '荃灣有俾俾人打死左',
+			exp: '發放假消息'
+		  }
+		]
+	  },
+	  {
+		uid: 9082,
+		name: 'Intel_King',
+		topics: [
+		  {
+			tid: 1346986,
+			link: 'https://lihkg.com/thread/1346986/page/1?ref=sharer&post=1',
+			topic: '連登仔又叫人唔好做警察了',
+			msg: '有事就識打999，對住警察就大大聲屌黑警黑警，狗，對自己冇幫助時就辱罵，拳打腳踢，要用嘅時候就係公僕，要聽你話，有事就鍾無艷，無事就夏迎春。你哋班生番玩撚晒啦！',
+			exp: '撐警'
+		  },
+		  {
+			tid: 1452575,
+			link: 'https://lihkg.com/thread/1452575/page/11?ref=sharer&post=255',
+			topic: '［緊急！］[眼中槍女仔] 我係佢啊妹 2',
+			msg: '樓主，好多人話佢係派錢蛇頭，你係佢阿妹，可否確認下相中人係咪同一個人呀？',
+			exp: '嘗試抹黑受害者'
+		  }
+		]
+	  },
+	  {
+		uid: 289792,
+		name: 'Sutherland',
+		topics: [
+		  {
+			tid: 1453481,
+			link: 'https://lihkg.com/thread/1453481/page/1?ref=sharer&post=1',
+			topic: '呢個系示威游行咩？呢個系恐怖襲擊！！！',
+			msg: '請共同捍衛我哋家，撐警察，嚴厲執法，一定要打擊啲暴力分子。',
+			exp: '撐警'
+		  }
+		]
+	  }
+	];
+}
